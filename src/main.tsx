c7fd223090ad37698781cfc42415fe7ffbb4bb73
Juanqui89 <Juanqui89.js@gmail.com>
2023-11-13 19:41:31 -0400

    Se actualizaron ciertos archivos

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/CSS/index.css";
 import "./assets/CSS/Queries.css";
+import { ThemeProvider} from "react-bootstrap";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ThemeProvider
+  breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+  minBreakpoint="xxs"
+>
     <App />
+</ThemeProvider>;
   </React.StrictMode>
 );
